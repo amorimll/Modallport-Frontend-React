c6051989d56de4126a74c81bd3a484abe77aa434
caicrs <caicrs.contact@gmail.com>
2023-06-23 11:33:01 -0300

    Routes - Operacional routes created

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,17 +1,30 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Processos from "../screens/Cadastros/Processos";
-import ModelosVistoria from "../screens/Cadastros/Vistoria";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Processos from '../screens/Cadastros/Processos';
+import ModelosVistoria from '../screens/Cadastros/Vistoria';
+import Operacional from '../screens/Operacional/Processos';
+import Vistorias from '../screens/Operacional/Processos/Vistorias';
+import RealizarConsulta from '../screens/Operacional/Processos/RealizarConsulta';
+import WelcomeMessage from '../screens/Homepage';
 
 const RoutesPaths = () => {
-    
-  return (
-    <BrowserRouter>
-        <Routes>
-          <Route path="/cadastros/processos" element={<Processos />}></Route>
-          <Route path="/cadastros/vistorias" element={<ModelosVistoria />}></Route>
-        </Routes>
-      </BrowserRouter>
-  )
-}
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path='/' element={<WelcomeMessage />}></Route>
+				<Route path='/cadastros/processos' element={<Processos />}></Route>
+				<Route
+					path='/cadastros/vistorias'
+					element={<ModelosVistoria />}></Route>
+				<Route path='/operacional/' element={<Operacional />}></Route>
+				<Route
+					path='/operacional/processos/:id'
+					element={<Vistorias />}></Route>
+				<Route
+					path='/operacional/consultas'
+					element={<RealizarConsulta />}></Route>
+			</Routes>
+		</BrowserRouter>
+	);
+};
 
-export default RoutesPaths
+export default RoutesPaths;

381b119b272621312aa8c8bc06bdd75175e7c60f
caicrs <caicrs.contact@gmail.com>
2023-06-23 11:41:13 -0300

    Page - Operacional/Processos created

---
+++ src/screens/Operacional/Processos/index.jsx
@@ -0,0 +1,67 @@
+// eslint-disable-next-line no-unused-vars
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './styles.css';
+import { useNavigate } from 'react-router';
+import Frame from '../../../components/Frame';
+
+const Operacional = () => {
+	const [processosData, setProcessosData] = useState([]);
+	const [loading, setLoading] = useState(true);
+	const navigate = useNavigate();
+
+	useEffect(() => {
+		const fetchData = async () => {
+			const res = await axios.get('https://localhost:44350/api/Processos');
+
+			if (res) {
+				setProcessosData(res.data);
+				setLoading(false);
+			}
+		};
+
+		fetchData();
+	}, []);
+
+	if (loading) {
+		return <>Loading...</>;
+	}
+
+	return (
+		<Frame>
+			<div className='processos container'>
+				<div className='processos-div' style={{ width: '100%' }}>
+					<div className='border-radius p-5 w-100'>
+						<table className='table'>
+							<thead>
+								<tr>
+									<th scope='col'>ID</th>
+									<th scope='col'>DESCRICAO</th>
+								</tr>
+							</thead>
+							<tbody id='operacional-list'>
+								{processosData.map((product) => (
+									<tr
+										key={product.idProcesso * Math.random() * 1000}
+										className='hover'
+										onClick={() => navigate('processos/' + product.idProcesso)}>
+										<td className='w-5'>{product.idProcesso}</td>
+										<td className='w-5'>{product.descricao}</td>
+									</tr>
+								))}
+							</tbody>
+							<thead>
+								<tr>
+									<th scope='col'>ID</th>
+									<th scope='col'>DESCRICAO</th>
+								</tr>
+							</thead>
+						</table>
+					</div>
+				</div>
+			</div>
+		</Frame>
+	);
+};
+
+export default Operacional;
